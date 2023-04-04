f1d26bd6561b0d46ffa0f5c3e470fbaad6f13512
Matthew Zember <matthew.zember@jupiterone.com>
2023-04-04 10:26:18 -0400

    add verifyAuth to client; add account step

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,10 @@
+import {
+  IntegrationError,
+  IntegrationProviderAPIError,
+  IntegrationProviderAuthenticationError,
+  IntegrationProviderAuthorizationError,
+} from '@jupiterone/integration-sdk-core';
+import { GaxiosError, GaxiosOptions, request } from 'gaxios';
 import { IntegrationConfig } from './config';
 
 export type ResourceIteratee<T> = (each: T) => Promise<void> | void;
@@ -14,7 +21,68 @@ export class APIClient {
   constructor(readonly config: IntegrationConfig) {}
 
   public async verifyAuthentication(): Promise<void> {
-    return Promise.resolve();
+    const url = '/api/v2/session/validate';
+    try {
+      await this.makeRequest({
+        url,
+        method: 'POST',
+        data: {
+          session: this.config.token,
+        },
+      });
+    } catch (err) {
+      throw new IntegrationProviderAuthenticationError({
+        cause: err,
+        endpoint: err.endpoint ?? '',
+        status: err.status ?? '',
+        statusText: err.statusText ?? '',
+      });
+    }
+  }
+
+  private async makeRequest(
+    opts: Pick<GaxiosOptions, 'url' | 'method' | 'data' | 'headers' | 'params'>,
+  ) {
+    try {
+      return await request({
+        baseUrl: this.config.baseUrl,
+        headers: {
+          session: this.config.token,
+        },
+        ...opts,
+      });
+    } catch (err) {
+      if (err instanceof GaxiosError) {
+        const status = err.response?.status ?? 404;
+        const statusText = err.response?.statusText ?? 'No Response Received';
+        const endpoint = err.response?.config?.url ?? this.config.baseUrl;
+
+        if (err.response?.status === 401) {
+          throw new IntegrationProviderAuthenticationError({
+            status,
+            statusText,
+            endpoint,
+          });
+        } else if (err.response?.status == 403) {
+          throw new IntegrationProviderAuthorizationError({
+            status,
+            statusText,
+            endpoint,
+          });
+        } else {
+          throw new IntegrationProviderAPIError({
+            status,
+            statusText,
+            endpoint,
+          });
+        }
+      } else {
+        throw new IntegrationError({
+          message: err.message,
+          code: (err as Error).name,
+        });
+      }
+    }
   }
 }
 
--- src/validateInvocation.test.ts
+++ src/validateInvocation.test.ts
@@ -28,7 +28,7 @@ describe('#validateInvocation', () => {
   /**
    * Testing a successful authorization can be done with recordings
    */
-  test.skip('successfully validates invocation', async () => {
+  test('successfully validates invocation', async () => {
     recording = setupProjectRecording({
       directory: __dirname,
       name: 'validate-invocation',
@@ -57,7 +57,7 @@ describe('#validateInvocation', () => {
       test.skip('should throw if clientId is invalid', async () => {
         recording = setupProjectRecording({
           directory: __dirname,
-          name: 'client-id-auth-error',
+          name: 'bad-token-error',
           // Many authorization failures will return non-200 responses
           // and `recordFailedRequest: true` is needed to capture these responses
           options: {
@@ -67,36 +67,15 @@ describe('#validateInvocation', () => {
 
         const executionContext = createMockExecutionContext({
           instanceConfig: {
-            baseUrl: 'https://example.com',
-            token: 'invalid-token',
+            ...integrationConfig,
+            token: 'bad-token',
           },
         });
 
         // tests validate that invalid configurations throw an error
         // with an appropriate and expected message.
         await expect(validateInvocation(executionContext)).rejects.toThrow(
-          'Provider authentication failed at https://localhost/api/v1/some/endpoint?limit=1: 401 Unauthorized',
-        );
-      });
-
-      test.skip('should throw if clientSecret is invalid', async () => {
-        recording = setupProjectRecording({
-          directory: __dirname,
-          name: 'client-secret-auth-error',
-          options: {
-            recordFailedRequests: true,
-          },
-        });
-
-        const executionContext = createMockExecutionContext({
-          instanceConfig: {
-            baseUrl: 'https://example.com',
-            token: 'invalid-token',
-          },
-        });
-
-        await expect(validateInvocation(executionContext)).rejects.toThrow(
-          'Provider authentication failed at https://localhost/api/v1/some/endpoint?limit=1: 401 Unauthorized',
+          /Provider authentication failed at https:\/\/[^\s/]+\/api\/v2\/session\/validate: 400 Bad Request/,
         );
       });
     });
